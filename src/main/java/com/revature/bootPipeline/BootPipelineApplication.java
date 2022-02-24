22fca0422aa026b064b0eef0c55b14f9b799ddcc
JesterCharles <charles.jester@revature.com>
2022-02-24 11:27:34 -0500

    Updated the pom and appliucation.yaml to work for us, also included the rest controller on the main driver

--- src/main/java/com/revature/bootPipeline/BootPipelineApplication.java
+++ src/main/java/com/revature/bootPipeline/BootPipelineApplication.java
@@ -2,10 +2,18 @@ package com.revature.bootPipeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BootPipelineApplication {
 
+	@GetMapping("/test")
+	public String testEndpoint() {
+		return "Hey from the CI/CD pipeline! He did the thing!";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BootPipelineApplication.class, args);
 	}
