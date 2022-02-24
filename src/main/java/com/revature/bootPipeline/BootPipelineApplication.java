9674732f4be5fb34e9e89837e1bda4a5e448706c
JesterCharles <charles.jester@revature.com>
2022-02-24 10:56:41 -0500

    Uploaded the bootPipeline demo to personal repo

---
+++ src/main/java/com/revature/bootPipeline/BootPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.revature.bootPipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/bootPipeline/BootPipelineApplicationTests.java
@@ -0,0 +1,20 @@
+package com.revature.bootPipeline;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+	@Test
+	void personalTest() {
+		assertTrue(true);
+	}
+
+}
