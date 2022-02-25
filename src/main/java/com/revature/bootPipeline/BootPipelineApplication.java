2191c4fcbba9cd3e81b9184d4ad8cecf7c423bf2
JesterCharles <charles.jester@revature.com>
2022-02-25 11:21:12 -0500

    Updated the bootPipeline to include counter

--- src/main/java/com/revature/bootPipeline/BootPipelineApplication.java
+++ src/main/java/com/revature/bootPipeline/BootPipelineApplication.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class BootPipelineApplication {
+	
+	private int counter;
 
 	@GetMapping("/test")
 	public String testEndpoint() {
-		return "Hey from the CI/CD pipeline! He did the thing! This is cool";
+		counter++;
+		return "Hey from the CI/CD pipeline! He did the thing! This is cool " + counter;
 	}
 	
 	public static void main(String[] args) {
